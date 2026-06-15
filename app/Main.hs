f1e0e5a498472f4348d2f7b80c2e5687fbd147d0
c0utin <rafaelcouto111@gmail.com>
2026-06-14 22:58:11 -0300

    minimal lexer

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,31 @@
 module Main where
 
+import Data.Char (isAlpha, isAlphaNum, isDigit, isSpace)
+import System.Environment (getArgs)
+
+data Token
+  = TIdent String
+  | TNumber String
+  | TSymbol Char
+  deriving (Show)
+
+tokenize :: String -> [Token]
+tokenize [] = []
+tokenize (c:cs)
+  | isSpace c = tokenize cs
+  | isAlpha c || c == '_' =
+      let (word, rest) = span (\x -> isAlphaNum x || x == '_') cs
+      in TIdent (c:word) : tokenize rest
+  | isDigit c =
+      let (num, rest) = span isDigit cs
+      in TNumber (c:num) : tokenize rest
+  | otherwise = TSymbol c : tokenize cs
+
 main :: IO ()
-main = putStrLn "Hello, Cabal!"
+main = do
+  args <- getArgs
+  case args of
+    [file] -> do
+      src <- readFile file
+      mapM_ print (tokenize src)
+    _ -> putStrLn "usage: normie-c <file>"
